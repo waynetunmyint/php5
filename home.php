cc8519cf03afce74586a06cb82342d9b36f288e0
Thet Win Htwe <thetwinhtwe@gmail.com>
2014-05-26 19:07:29 +0630

    Kothet:I add Ko_thet function

--- home.php
+++ home.php
@@ -1,20 +1,11 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
 	function KMH_function()
 	{
-=======
 
 	function kothet_fuction()
 	{
 	}
-	
-	?>
->>>>>>> 62c8805bf9d671b3c2bc1df5428d64e5d947c004
-=======
 	function daniel_function(){
->>>>>>> 5110416d8e643f9ea896cd46b7d131b09f7c2005
-
 	}
 ?>
->>>>>>> 5110416d8e643f9ea896cd46b7d131b09f7c2005
+
